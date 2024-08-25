b3b18632f31c8ab3a2c33e50f8709840fafdcf6a
Glitch (aframe) <none>
2024-08-25 13:26:11 +0000

    💈🎎 Checkpoint ./layout.js:44877082/2996

--- layout.js
+++ layout.js
@@ -1,16 +1,20 @@
 const sceneEl = document.querySelector("a-scene");
 const y = 1.5;
+const dis = 5;
 
-let cubeEl = document.createElement("a-box");
-cubeEl.setAttribute("color", "blue");
-cubeEl.setAttribute("position", "0 1.5 -2");
-sceneEl.appendChild(cubeEl);
+createElement(0, -2, "tryC", "tryId", "nosound");
 
-
-function createElement(x, z, c, id){
+function createElement(x, z, c, id, s) {
   const sphereEl = document.createElement("a-sphere");
   sphereEl.setAttribute("color", "#EF2D5E");
-  sphereEl.setAttribute("position", x + ' ' + y +' ' + )
+  sphereEl.setAttribute("shader", "flat");
+  sphereEl.setAttribute("radius", "0.5");
+  sphereEl.setAttribute("position", x + " " + y + " " + z);
+  sphereEl.setAttribute("class", c);
+  sphereEl.setAttribute("id", id);
+  sphereEl.setAttribute("sound", "src:" + s);
+  
+  sceneEl.appendChild(sphereEl);
 }
 
 function distance(x1, z1, x2, z2) {
