59f33d6bfdaef8e4ffb46fbee954cd89b060a88c
Glitch (aframe) <none>
2024-09-04 13:17:38 +0000

    🍥✨ Checkpoint ./layout.js:44877082/154

--- layout.js
+++ layout.js
@@ -124,20 +124,20 @@ function iterateSection(x, y, z, d, section, ele, prename, angle) {
       createElement(el, xp, y, zp, "#FFFF00", classP, idP, nameP);
     }
 
-    if (section[key].Subsections) {
-      // console.log(key);
-      iterateSection(
-        x1,
-        y,
-        z1,
-        d2,
-        section[key].Subsections,
-        el,
-        name + "_Subsections_",
-        0
-        // deg * i - 0.5 * Math.PI
-      );
-    }
+    // if (section[key].Subsections) {
+    //   // console.log(key);
+    //   iterateSection(
+    //     x1,
+    //     y,
+    //     z1,
+    //     d2,
+    //     section[key].Subsections,
+    //     el,
+    //     name + "_Subsections_",
+    //     0
+    //     // deg * i - 0.5 * Math.PI
+    //   );
+    // }
     i++;
   }
 }
